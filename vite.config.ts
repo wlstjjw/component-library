dfde37be54ba34e91d8bfb27fd0ba6a085934e88
jinseo <jinseo.choi@interxlab.com>
2025-12-02 15:09:22 +0900

    초기 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: ["@emotion"],
+    },
+  })],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
+    ],
+  },
 })
