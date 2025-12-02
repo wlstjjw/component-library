51df5d735087a90cb83faa82d8a3f929115e5e92
jinseo <jinseo.choi@interxlab.com>
2025-12-03 08:13:39 +0900

    add globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Badge from '@/components/jinseo/Badge'
+import { Global } from "@emotion/react";
+import { globalStyle } from "@/styles/globalStyle";
 
 function App() {
   return (
     <>
+      <Global styles={globalStyle()} />
       <Badge />
     </>
   )
